279934f2cba2b1d815c2348944ea0921d923a5b9
maayeesha <4326526-maayeesha@users.noreply.replit.com>
2024-03-07 11:27:47 +0000

    Added Encapsulation in Principles

--- .ccls-cache/@home@runner@object-oriented-programming-1/main.cpp
+++ .ccls-cache/@home@runner@object-oriented-programming-1/main.cpp
@@ -3,10 +3,23 @@ using namespace std;
 
 class Employee {
   // private:
-public:
   string Name;
   string Company;
   int Age;
+
+public:
+  // setter
+  void setName(string name) { Name = name; }
+  // getter
+  string getName() { return Name; }
+  // setter
+  void setCompany(string company) { Company = company; }
+  // getter
+  string getCompany() { return Company; }
+  // setter
+  void setAge(int age) { Age = age; }
+  // getter
+  int getAge() { return Age; }
 
   void Introduceyourself() {
     cout << "Name: " << Name << endl;
@@ -30,4 +43,8 @@ int main() {
   // employee1.Introduceyourself();
   Employee employee2 = Employee("Farzana", "XYZ", 25);
   employee2.Introduceyourself();
+
+  employee1.setAge(50);
+  cout << employee1.getName() << " is " << employee1.getAge() << " years old."
+       << endl;
 }
