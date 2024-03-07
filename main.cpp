0bd1228ab8434be5c6823cd2221755a0f9570ce8
Maayeesha Farzana <63303436+maayeesha@users.noreply.github.com>
2024-03-07 10:41:02 +0000

    OOP basics

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Employee {
+  // private:
+public:
+  string Name;
+  string Company;
+  int Age;
+
+  void Introduceyourself() {
+    cout << "Name: " << Name << endl;
+    cout << "Company: " << Company << endl;
+    cout << "Age: " << Age << endl;
+  }
+
+  Employee(string name, string company, int age) {
+    Name = name;
+    Company = company;
+    Age = age;
+  }
+};
 
 int main() {
-  std::cout << "Hello World!\n";
+  Employee employee1 = Employee("Maayeesha", "ABC", 24);
+  employee1.Introduceyourself();
+  // employee1.Name = "Maayeesha";
+  // employee1.Company = "ABC";
+  // employee1.Age = 24;
+  // employee1.Introduceyourself();
+  Employee employee2 = Employee("Farzana", "XYZ", 25);
+  employee2.Introduceyourself();
 }
