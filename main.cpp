ce0ea9b2a92627dbd23b0621090efa8a06a01269
maayeesha <4326526-maayeesha@users.noreply.replit.com>
2024-03-07 14:27:02 +0000

    Abstraction Added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Employee {
+class AbstractEmployee {
+  virtual void
+  askForPromotion() = 0; // whoever signs for the abstractemployee contract will
+                         // have to provide impmentation for this method hence
+                         // "virtual"
+};
+
+class Employee : AbstractEmployee {
   // private:
   string Name;
   string Company;
@@ -17,7 +24,10 @@ public:
   // getter
   string getCompany() { return Company; }
   // setter
-  void setAge(int age) { Age = age; }
+  void setAge(int age) {
+    if (age >= 18)
+      Age = age;
+  }
   // getter
   int getAge() { return Age; }
 
@@ -32,19 +42,34 @@ public:
     Company = company;
     Age = age;
   }
+  void askForPromotion() {
+    if (Age > 30) {
+      cout << Name << " got promoted" << endl;
+    } else
+      cout << Name << " ,Sorry, no promotion for you" << endl;
+  }
 };
 
+class Developer : Employee {
+  string favouriteProgrammingLanguage;
+};
 int main() {
   Employee employee1 = Employee("Maayeesha", "ABC", 24);
-  employee1.Introduceyourself();
+  // employee1.Introduceyourself();
   // employee1.Name = "Maayeesha";
   // employee1.Company = "ABC";
   // employee1.Age = 24;
   // employee1.Introduceyourself();
-  Employee employee2 = Employee("Farzana", "XYZ", 25);
-  employee2.Introduceyourself();
+  Employee employee2 = Employee("Farzana", "XYZ", 35);
+  // employee2.Introduceyourself();
 
-  employee1.setAge(50);
-  cout << employee1.getName() << " is " << employee1.getAge() << " years old."
-       << endl;
+  // Encapsulation
+  //  employee1.setAge(12);
+  //  cout << employee1.getName() << " is " << employee1.getAge() << " years
+  //  old."
+  //       << endl;
+
+  // Abstraction
+  employee1.askForPromotion();
+  employee2.askForPromotion();
 }
